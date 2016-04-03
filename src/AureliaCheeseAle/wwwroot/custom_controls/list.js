b75d357eaeccf68af02197876bef07ac809155d0
tommyly <tommyly764@gmail.com>
2016-04-03 20:47:34 +0100

    Adds dependency injection for list view

---
+++ src/AureliaCheeseAle/wwwroot/custom_controls/list.js
@@ -0,0 +1,19 @@
+﻿import { inject } from 'aurelia-framework';
+import { SurveyRetriever } from '../services/SurveyRetriever';
+
+let getUrl = 'http://localhost:59346/surveys';
+
+@inject(SurveyRetriever)
+export class List {
+    surveys = [];
+    
+    constructor(surveyRetriever) {
+
+        this.surveyRetriever = surveyRetriever;
+
+        this.surveyRetriever.getAll()
+                            .then(surveys => { 
+                                this.surveys = surveys; 
+                            });
+    }
+}
